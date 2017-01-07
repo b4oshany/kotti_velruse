de2bdf3fda4cb260f239908e02de5f34e4d7d5af
Oshane Bailey <b4.oshany@gmail.com>
2017-01-07 13:43:38 -0500

    Add pyramid_mako to pyramid_includes

--- kotti_velruse/__init__.py
+++ kotti_velruse/__init__.py
@@ -1,9 +1,12 @@
-__version__ = '0.3.4'
+__version__ = '0.3.5'
 
 
 log = __import__('logging').getLogger(__name__)
 
 
 def kotti_configure(settings):
-    settings['pyramid.includes'] += ' velruse.app'
-    settings['pyramid.includes'] += ' kotti_velruse.views'
+    settings['pyramid.includes'] += (
+        ' velruse.app'
+        ' pyramid_mako'
+        ' kotti_velruse.views'
+    )
